4d91a37be6e8a9e230a1e25c081c585837bcd749
Adam Cichý <adam@MacBook-Air-de-Adam.local>
2017-12-14 15:22:59 +0100

    scroll jquery

--- js/styles.js
+++ js/styles.js
@@ -5,3 +5,11 @@
         function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
         }
+$(window).scroll(function() {
+
+        if ($(window).scrollTop() > 10) {
+            $('#desktop-nav').addClass('bckgrnd-col');
+        } else {
+            $('#desktop-nav').removeClass('bckgrnd-col');
+        }
+    });
